315bcb8c66d7ac9bf3b94425113a7b9f31d7b21e
TheKruger <60050026+YES-YES-Kruger@users.noreply.github.com>
2021-02-15 00:43:17 +0100

    test_main.py removed

--- test_main.py
+++
@@ -1,8 +0,0 @@
-import OpenPS
-
-event = OpenPS.Event()
-
-event.init() # Initialize the events otherwise the built-in events will won't works.
-
-event.create("OnProgramFoo") # Create event.
-event.call("OnProgramFoo") # Call the event.
