df9f3df40c066844528a56ef9e2bedb61aed289b
TheKruger <60050026+YES-YES-Kruger@users.noreply.github.com>
2021-02-15 01:44:45 +0100

    Setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='OpenPS',
+   version='0.0.1',
+   description='Open source plugin system.',
+   long_description=long_description,
+   author='TheKruger',
+   keywords=["python", "plugin-system"]
+   packages=['OpenPS']
+)
