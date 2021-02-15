8f3701d7be25d7500a7a25ed223cdf4d281d315d
TheKruger <nemesdaniel69@gmail.com>
2021-02-15 20:17:50 +0100

    License and github link added to the setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setup(
    description='Open source plugin system.',
    long_description_content_type="text/markdown",
    long_description=long_description,
+   license="Apache-2.0",
+   dependency_links=["https://github.com/TheKruger/OpenPS"],
    author='TheKruger',
    keywords=["python", "plugin-system"],
    packages=['OpenPS']
